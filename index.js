fd5ce599fa811762936d3d9b5ba5c70140081c57
Shobhit Ratan <shobhit.ratan@baruchmail.cuny.edu>
2020-02-16 22:43:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function shout(string) {
-  return string.toUpperCase()
+  return string.toUpperCase();
 }
 
 function whisper(string) {
-  return string.toLowerCase()
+  return string.toLowerCase();
 }
 
 function logShout(string) {
@@ -12,4 +12,10 @@ function logShout(string) {
 
 function logWhisper(string) {
   console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() == string) {
+    return "I can't hear you!";
+  }
 }
